5f09722e150419751517c7b35286451c8502c1d1
Erik Storås Sommer <erikssommer@gmail.com>
2022-10-27 22:29:51 +0200

    For loop intro

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,9 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+
+    // a) Generate an array containing the values 1 up to and including 50 using for loop
+    val array = for (i <- 1 to 50) yield i
+    println(array.mkString(", "))
   }
 }
