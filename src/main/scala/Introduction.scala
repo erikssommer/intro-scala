da14be4e2c64976b8ca7be5d076832b6f3410d17
Erik Storås Sommer <erikssommer@gmail.com>
2022-10-27 23:20:40 +0200

    Creating classes

---
+++ src/main/scala/Introduction.scala
@@ -0,0 +1,27 @@
+class Introduction {
+  def sum(array: Array[Int]): Int = {
+    var sum = 0
+    for (i <- array) {
+      sum += i
+    }
+    sum
+  }
+
+  def sumRec(array: Array[Int]): Int = {
+    if (array.length == 0) {
+      0
+    } else {
+      array(0) + sumRec(array.drop(1))
+    }
+  }
+
+  def fib(value: Int): BigInt = {
+    if (value == 0) {
+      0
+    } else if (value == 1) {
+      1
+    } else {
+      fib(value - 1) + fib(value - 2)
+    }
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,34 +5,20 @@ object Main {
     println("Hello world!")
 
     // Task 1: Scala Introduction
-
+    val introduction = new Introduction()
     // a) Generate an array containing the values 1 up to and including 50 using for loop
     val array = for (i <- 1 to 50) yield i
     println(array.mkString(", "))
 
     // b) Create a function that sums the elements in an array of integers using a for loop
-    def sum(array: Array[Int]): Int = {
-      var sum = 0
-      for (i <- array) {
-        sum += i
-      }
-      sum
-    }
 
     // Testing with array from a)
-    println("Sum using for loop: " + sum(array.toArray))
+    println("Sum using for loop: " + introduction.sum(array.toArray))
 
     // c) Create a function that sums the elements in an array of integers using recursion
-    def sumRec(array: Array[Int]): Int = {
-      if (array.length == 0) {
-        0
-      } else {
-        array(0) + sumRec(array.drop(1))
-      }
-    }
 
     // Testing with array from a)
-    println("Sum using recursion: " + sumRec(array.toArray))
+    println("Sum using recursion: " + introduction.sumRec(array.toArray))
 
     // Create a function to compute the nth Fibonacci number using recursion without using memoization (or other optimizations).
     // Use BigInt instead of Int.
@@ -40,18 +26,8 @@ object Main {
 
     // The difference between Int and BigInt is that Int is a 32-bit signed integer,
     // while BigInt is an arbitrary-precision integer.
-    def fib(value: Int) : BigInt = {
-      if (value == 0) {
-        0
-      } else if (value == 1) {
-        1
-      } else {
-        fib(value - 1) + fib(value - 2)
-      }
-    }
-
     // Testing with n = 9
-    println("Fibonacci number of 9: " + fib(9))
+    println("Fibonacci number of 9: " + introduction.fib(9))
 
     // Task2: Concurrency in Scala
 
@@ -63,7 +39,22 @@ object Main {
       }
       t
     }
-    // Testing with a simple function
-    val t = thread { println("Hello from a thread!") }
+
+    // b) Given the following code snippet:
+    private var counter: Int = 0
+    def increaseCounter(): Unit = {
+      counter += 1
+    }
+
+    // Create a function that prints the current counter variable.
+    def printCounter(): Unit = {
+      println(counter)
+    }
+    // Start three threads, two that initialize increaseCounter
+
+
+
+
+
   }
 }
