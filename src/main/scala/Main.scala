523ca1ba55c6c56ccb7188eb26d562f2ece9fb6c
Erik Storås Sommer <erikssommer@gmail.com>
2022-10-27 22:33:15 +0200

    Function intro

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,5 +5,17 @@ object Main {
     // a) Generate an array containing the values 1 up to and including 50 using for loop
     val array = for (i <- 1 to 50) yield i
     println(array.mkString(", "))
+
+    // b) Create a function that sums the elements in an array of integers using a for loop
+    def sum(array: Array[Int]): Int = {
+      var sum = 0
+      for (i <- array) {
+        sum += i
+      }
+      sum
+    }
+
+    // Testing with array from a)
+    println(sum(array.toArray))
   }
 }
